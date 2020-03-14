522a39551353a26381fd46fe7fafc0540cfd1a5f
unknown <michael.vigerl@web.de>
2020-03-14 20:26:41 +0100

    turn off servo over and under min/max temp

--- heizungsteuerung/heizungsteuerung.ino
+++ heizungsteuerung/heizungsteuerung.ino
@@ -1,16 +1,22 @@
 #include <Arduino.h>
 #include <U8x8lib.h>
+#include <Servo.h>
+
 
 //display
 U8X8_SSD1306_128X64_NONAME_HW_I2C u8x8(U8X8_PIN_NONE);
 
 //pins
 const int ThermistorPin = 0;
-const int UpMinPin = 2;
-const int DownMinPin = 3;
-const int UpMaxPin = 4;
-const int DownMaxPin = 5;
+const int Button2 = 2;
+const int Button3 = 3;
+const int Button4 = 4;
+const int Button5 = 5;
+
 const int ServoPin = 9;
+
+const int PinGnd=6;
+const int PinVcc=7;
 
 //settings
 int maxTemp = 25;
@@ -22,26 +28,38 @@ float R1 = 10000;
 float logR2, R2, Tk, currentTemp;
 float c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07;
 
+//Servo vars
+int pos = 0;
+Servo heaterServo;
 
-void setup(void)
-{
-  pinMode(UpMinPin, INPUT);
-  pinMode(DownMinPin, INPUT);
-  pinMode(UpMaxPin, INPUT);
-  pinMode(DownMaxPin, INPUT);
-
+void setup(void) {
   Serial.begin(9600);
+
   u8x8.begin();
   u8x8.setPowerSave(0);
+
+  pinMode(Button2, INPUT);
+  pinMode(Button3, INPUT);
+  pinMode(Button4, INPUT);
+  pinMode(Button5, INPUT);
+
+  heaterServo.attach(ServoPin);
+
+  //Use these pins as additional vcc and gnd pins
+  pinMode(PinGnd, OUTPUT);
+  pinMode(PinVcc, OUTPUT);
+  digitalWrite(PinGnd,LOW);
+  digitalWrite(PinVcc,HIGH);
 }
 
 
 void loop(void)
 {
-  Serial.println("loopi");
   get_temp();
   get_user_input();
+  calc_servo_pos();
   draw_overview();
+  Serial.println(currentTemp, pos);
   delay(2000);
 }
 
@@ -57,26 +75,39 @@ void get_temp() {
 
 void get_user_input() {
   //min buttons
-  if (digitalRead(UpMinPin) == HIGH) {
+  if (digitalRead(Button2) == HIGH) {
     if (minTemp < maxTemp) {
       minTemp++;
     }
   }
-  if (digitalRead(DownMinPin) == HIGH) {
+  if (digitalRead(Button3) == HIGH) {
     minTemp--;
   }
 
   //max buttons
-  if (digitalRead(UpMaxPin) == HIGH) {
+  if (digitalRead(Button4) == HIGH) {
     maxTemp++;
   }
-  if (digitalRead(DownMaxPin) == HIGH) {
+  if (digitalRead(Button5) == HIGH) {
     if(maxTemp>minTemp){
     maxTemp--;
     }
   }
 }
 
+void calc_servo_pos(){
+  if(pos<=90){ //off
+    if(currentTemp<minTemp){
+      pos=180;
+    }
+  }
+  if(pos>90){ //on
+    if(currentTemp>maxTemp){
+      pos=0;
+    }
+  }
+  heaterServo.write(pos);
+}
 
 void draw_overview() {
   u8x8.setFont(u8x8_font_7x14B_1x2_f);
